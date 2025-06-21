66936fc67e5d0bfc479c168d046c74bc1ab96aa5
Uday Kumar Chunduru <40311746+UdayKumarChunduru@users.noreply.github.com>
2025-06-21 22:38:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ repo init -u https://github.com/ProjectInfinity-X/manifest -b 15 --git-lfs; \
 # Step 4: Remove Old Device, Kernel, and Vendor Trees
 # ----------------------------------------
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250; \
-rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
+rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf hardware/dolby && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
 
 # ----------------------------------------
@@ -64,12 +64,17 @@ git clone https://github.com/UdayKumarChunduru/android_hardware_dolby hardware/d
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 
 # ----------------------------------------
-# Step 12: Clone MIUI Camera
+# Step 12: Clone ViPER4AndroidFX
+# ----------------------------------------
+git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
+
+# ----------------------------------------
+# Step 13: Clone MIUI Camera
 # ----------------------------------------
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
 # ----------------------------------------
-# Step 13: Setup Build Environment and Start Build
+# Step 14: Setup Build Environment and Start Build
 # ----------------------------------------
-. build/envsetup.sh; \
+source build/envsetup.sh; \
 lunch infinity_munch-user && mka bacon; \
